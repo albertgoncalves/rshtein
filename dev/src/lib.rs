3bd1346ea0d2f1f52658b1c34eeb6ee671c1714b
albertgoncalves <goncalves.albert@gmail.com>
2020-01-15 00:03:50 -0500

    array benchmarks, arrayvec is king again tho

--- dev/src/lib.rs
+++ dev/src/lib.rs
@@ -3,6 +3,8 @@
 use arrayvec::ArrayVec;
 use unchecked_index::{unchecked_index, UncheckedIndex};
 
+const CAPACITY: usize = 4096;
+
 /* NOTE: _.as_bytes() is fine as long as the given strings only contain symbols
  * between U+0000 and U+007F.
  */
@@ -48,8 +50,8 @@ pub fn lev_2d_vec(a: &str, b: &str) -> usize {
     for j in 1..width {
         matrix[0][j] = j;
     }
-    for j in 1..width {
-        for i in 1..height {
+    for i in 1..height {
+        for j in 1..width {
             let penalty: usize = {
                 if a[i - 1] == b[j - 1] {
                     0
@@ -85,8 +87,8 @@ pub fn lev_1d_vec(a: &str, b: &str) -> usize {
     for j in 1..width {
         matrix[j] = j;
     }
-    for j in 1..width {
-        for i in 1..height {
+    for i in 1..height {
+        for j in 1..width {
             let penalty: usize = {
                 if a[i - 1] == b[j - 1] {
                     0
@@ -102,8 +104,6 @@ pub fn lev_1d_vec(a: &str, b: &str) -> usize {
     matrix[n - 1]
 }
 
-const CAPACITY: usize = 4096;
-
 #[must_use]
 #[allow(clippy::needless_range_loop)]
 pub fn lev_1d_arrayvec(a: &str, b: &str) -> usize {
@@ -112,30 +112,95 @@ pub fn lev_1d_arrayvec(a: &str, b: &str) -> usize {
     let height: usize = a.len() + 1;
     let width: usize = b.len() + 1;
     let n: usize = height * width;
-    /* NOTE: With small inputs, this means of initialization will allow the
-     * `ArrayVec` implementations to beat `lev_1d_vec`. As input size grows,
-     * this operation falls behind `lev_1d_vec`'s `vec![...]`.  Initializing
-     * `matrix` via `...from([0; CAPACITY])` seems to cause the implementations
-     * to trade places: `ArrayVec` now wins for large inputs, `Vec` for small
-     * inputs.
-     */
     let mut matrix: ArrayVec<[usize; CAPACITY]> = ArrayVec::new();
-    for _ in 0..n {
-        matrix.push(0);
-    }
     macro_rules! select {
         ($j:expr, $i:expr $(,)?) => {
             $j + ($i * width)
         };
     }
+    matrix.push(0);
+    for j in 1..width {
+        matrix.push(j);
+    }
     for i in 1..height {
-        matrix[i * width] = i;
+        matrix.push(i);
+        for j in 1..width {
+            let penalty: usize = {
+                if a[i - 1] == b[j - 1] {
+                    0
+                } else {
+                    1
+                }
+            };
+            let value: usize = (matrix[select!(j, i - 1)] + 1)
+                .min(matrix[select!(j - 1, i)] + 1)
+                .min(matrix[select!(j - 1, i - 1)] + penalty);
+            matrix.push(value);
+        }
+    }
+    matrix[n - 1]
+}
+
+#[must_use]
+#[allow(clippy::missing_safety_doc, clippy::needless_range_loop)]
+pub unsafe fn lev_1d_arrayvec_unsafe(a: &str, b: &str) -> usize {
+    let a: &[u8] = a.as_bytes();
+    let b: &[u8] = b.as_bytes();
+    let height: usize = a.len() + 1;
+    let width: usize = b.len() + 1;
+    let n: usize = height * width;
+    assert!(n < CAPACITY);
+    let mut matrix: UncheckedIndex<ArrayVec<[usize; CAPACITY]>> =
+        unchecked_index(ArrayVec::new());
+    macro_rules! select {
+        ($j:expr, $i:expr $(,)?) => {
+            $j + ($i * width)
+        };
+    }
+    matrix.push_unchecked(0);
+    for j in 1..width {
+        matrix.push_unchecked(j);
+    }
+    for i in 1..height {
+        matrix.push_unchecked(i);
+        for j in 1..width {
+            let penalty: usize = {
+                if a[i - 1] == b[j - 1] {
+                    0
+                } else {
+                    1
+                }
+            };
+            let value: usize = (matrix.get_unchecked(select!(j, i - 1)) + 1)
+                .min(matrix.get_unchecked(select!(j - 1, i)) + 1)
+                .min(matrix.get_unchecked(select!(j - 1, i - 1)) + penalty);
+            /* NOTE: `push_unchecked` here is *slower*. Why? How? */
+            matrix.push(value);
+        }
+    }
+    matrix[n - 1]
+}
+
+#[must_use]
+#[allow(clippy::needless_range_loop)]
+pub fn lev_1d_array(a: &str, b: &str) -> usize {
+    let a: &[u8] = a.as_bytes();
+    let b: &[u8] = b.as_bytes();
+    let height: usize = a.len() + 1;
+    let width: usize = b.len() + 1;
+    let n: usize = height * width;
+    let mut matrix: [usize; CAPACITY] = [0; CAPACITY];
+    macro_rules! select {
+        ($j:expr, $i:expr $(,)?) => {
+            $j + ($i * width)
+        };
     }
     for j in 1..width {
         matrix[j] = j;
     }
-    for j in 1..width {
-        for i in 1..height {
+    for i in 1..height {
+        matrix[i * width] = i;
+        for j in 1..width {
             let penalty: usize = {
                 if a[i - 1] == b[j - 1] {
                     0
@@ -153,31 +218,26 @@ pub fn lev_1d_arrayvec(a: &str, b: &str) -> usize {
 
 #[must_use]
 #[allow(clippy::missing_safety_doc, clippy::needless_range_loop)]
-pub unsafe fn lev_1d_arrayvec_unsafe(a: &str, b: &str) -> usize {
+pub unsafe fn lev_1d_array_unsafe(a: &str, b: &str) -> usize {
     let a: &[u8] = a.as_bytes();
     let b: &[u8] = b.as_bytes();
     let height: usize = a.len() + 1;
     let width: usize = b.len() + 1;
     let n: usize = height * width;
     assert!(n < CAPACITY);
-    let mut matrix: UncheckedIndex<ArrayVec<[usize; CAPACITY]>> =
-        unchecked_index(ArrayVec::new());
-    for _ in 0..n {
-        matrix.push_unchecked(0);
-    }
+    let mut matrix: UncheckedIndex<[usize; CAPACITY]> =
+        unchecked_index([0; CAPACITY]);
     macro_rules! select {
         ($j:expr, $i:expr $(,)?) => {
             $j + ($i * width)
         };
     }
-    for i in 1..height {
-        matrix[i * width] = i;
-    }
     for j in 1..width {
         matrix[j] = j;
     }
-    for j in 1..width {
-        for i in 1..height {
+    for i in 1..height {
+        matrix[i * width] = i;
+        for j in 1..width {
             let penalty: usize = {
                 if a[i - 1] == b[j - 1] {
                     0
@@ -210,7 +270,13 @@ mod tests {
         }};
     }
 
-    macro_rules! bench_case {
+    macro_rules! bench_case_short {
+        ($b:expr, $fn:expr $(,)?) => {
+            $b.iter(|| $fn("sitting", "kitten"))
+        };
+    }
+
+    macro_rules! bench_case_long {
         ($b:expr, $fn:expr $(,)?) => {
             $b.iter(|| {
                 $fn(
@@ -222,49 +288,93 @@ mod tests {
     }
 
     macro_rules! test_and_bench {
-        ($fn:expr, $test:ident, $bench:ident $(,)?) => {
+        (
+            $fn:expr,
+            $test:ident,
+            $bench_short:ident,
+            $bench_long:ident $(,)?
+        ) => {
             #[test]
             fn $test() {
                 test_cases!($fn)
             }
 
             #[bench]
-            fn $bench(b: &mut Bencher) {
-                bench_case!(b, $fn)
+            fn $bench_short(b: &mut Bencher) {
+                bench_case_short!(b, $fn)
+            }
+
+            #[bench]
+            fn $bench_long(b: &mut Bencher) {
+                bench_case_long!(b, $fn)
             }
         };
     }
 
     macro_rules! test_and_bench_unsafe {
-        ($fn:expr, $test:ident, $bench:ident $(,)?) => {
+        (
+            $fn:expr,
+            $test:ident,
+            $bench_short:ident,
+            $bench_long:ident $(,)?
+        ) => {
             #[test]
             fn $test() {
                 unsafe { test_cases!($fn) }
             }
 
             #[bench]
-            fn $bench(b: &mut Bencher) {
-                unsafe { bench_case!(b, $fn) }
+            fn $bench_short(b: &mut Bencher) {
+                unsafe { bench_case_short!(b, $fn) }
+            }
+
+            #[bench]
+            fn $bench_long(b: &mut Bencher) {
+                unsafe { bench_case_long!(b, $fn) }
             }
         };
     }
 
-    /* NOTE: `lev_recursive` is too slow for the target benchmark. */
+    /* NOTE: `lev_recursive` is too slow for the long benchmark. */
     #[test]
     fn test_lev_recursive() {
         test_cases!(lev_recursive)
     }
 
-    test_and_bench!(lev_2d_vec, test_lev_2d_vec, bench_lev_2d_vec);
-    test_and_bench!(lev_1d_vec, test_lev_1d_vec, bench_lev_1d_vec);
+    test_and_bench!(
+        lev_2d_vec,
+        test_lev_2d_vec,
+        bench_lev_2d_vec_short,
+        bench_lev_2d_vec_long,
+    );
+    test_and_bench!(
+        lev_1d_vec,
+        test_lev_1d_vec,
+        bench_lev_1d_vec_short,
+        bench_lev_1d_vec_long,
+    );
     test_and_bench!(
         lev_1d_arrayvec,
         test_lev_1d_arrayvec,
-        bench_lev_1d_arrayvec,
+        bench_lev_1d_arrayvec_short,
+        bench_lev_1d_arrayvec_long,
     );
     test_and_bench_unsafe!(
         lev_1d_arrayvec_unsafe,
         test_lev_1d_arrayvec_unsafe,
-        bench_lev_1d_arrayvec_unsafe,
+        bench_lev_1d_arrayvec_unsafe_short,
+        bench_lev_1d_arrayvec_unsafe_long,
+    );
+    test_and_bench!(
+        lev_1d_array,
+        test_lev_1d_array,
+        bench_lev_1d_array_short,
+        bench_lev_1d_array_long,
+    );
+    test_and_bench_unsafe!(
+        lev_1d_array_unsafe,
+        test_lev_1d_array_unsafe,
+        bench_lev_1d_array_unsafe_short,
+        bench_lev_1d_array_unsafe_long,
     );
 }
